4729a6c130b614e373967d79f8e713ef7fb900fc
urumak <13crumak@gmail.com>
2021-06-09 21:05:32 +0200

    Configure API

---
+++ Library/Library.Api/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Library.Data.EF;
+using Library.Data.EF.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Add(Book.Create("Lśnienie", Name.Create("Stephen", "King", null).Value).Value);
+                context.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
--- Library/Library.Api/Startup.cs
+++ Library/Library.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Library.Data.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library.Api
 {
@@ -26,7 +28,6 @@ namespace Library.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
